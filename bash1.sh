3303c9cae80e001acc537067df805cec7f09b8dc
Luisa Mao <luisa.maocp@gmail.com>
2022-09-11 13:11:04 -0500

    commit

--- bash1.sh
+++ bash1.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 # export rosbag_path="$(find / -name "spot_ahg_dataset" 2>/dev/null)"
 # export rosbag_path=/hdd/luisamao/robodata/spot_ahg_dataset
-export rosbag_path=/hdd/luisamao/robodata/Large_Crowds_Data
+export rosbag_path=/hdd/luisamao/robodata/Large_Crowds_Data1
 export num=${1:-10} 
 
-echo 'starting python'
+echo 'writing rosbags to rosbags.txt'
 
 # python3 scripts/get_rosbags.py
 
-# PYCMD=$(cat <<EOF
-# import os
-# rosbag_path = os.environ['rosbag_path']
-# # iterate over files in
-# # that directory
-# num=0
-# with open('rosbags.txt', 'w') as file:
-#     for filename in os.listdir(rosbag_path):
-#         f = os.path.join(rosbag_path, filename)
-#         # checking if it is a file
-#         if os.path.isfile(f):
-#             file.write(filename.replace('.bag',''))
-#             file.write('\n')
-#         num+=1
-#         if num==int(os.environ['num']):
-#             break
-# EOF
-# )
+PYCMD=$(cat <<EOF
+import os
+rosbag_path = os.environ['rosbag_path']
+# iterate over files in
+# that directory
+num=0
+with open('rosbags.txt', 'w') as file:
+    for filename in os.listdir(rosbag_path):
+        f = os.path.join(rosbag_path, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            file.write(filename.replace('.bag',''))
+            file.write('\n')
+        num+=1
+        if num==int(os.environ['num']):
+            break
+EOF
+)
 
-# python3 -c "$PYCMD"
+python3 -c "$PYCMD"
 
-echo 'starting rosbags.txt'
+echo 'parsing bags in rosbags.txt'
 
 File="rosbags.txt"
 Lines=$(cat $File)
@@ -39,11 +39,14 @@ do
     roslaunch lidar_rosbag_parser parse_rosbag.launch bag:=$Line
 done
 
+echo 'done'
+
 echo 'starting sftp'
 
 sftp -oPort=22 luisamao@robovision.csres.utexas.edu:/robodata/luisamao <<EOF
-# rm -r Preference_Learning_Data
-# put -R /hdd/luisamao/robodata/Preference_Learning_Data 
+# # rm -r Preference_Learning_Data
+# # put -R /hdd/luisamao/robodata/Preference_Learning_Data 
+rm -r Nav_Large_Crowds_Data
 put -R /hdd/luisamao/robodata/Nav_Large_Crowds_Data
 exit
 EOF
---
+++ scripts/check_trajectories.py
@@ -0,0 +1,32 @@
+import pickle
+import numpy as np
+import os
+import sys
+
+if len(sys.argv)>1:
+    directory = sys.argv[1]
+else: directory = "/hdd/luisamao/robodata/Nav_Large_Crowds_Data"
+count = 0
+total = 0
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        # print(f)
+        with open(f, "rb") as file:
+            a = pickle.load(file)
+            for index in range(0, len(a['human_expert_odom'])):
+                if (len(a['move_base_path'][index])>0):
+                    x1 = a['human_expert_odom'][index][0][0]-a['move_base_path'][index][0][0]
+                    y1 = a['human_expert_odom'][index][0][1]-a['move_base_path'][index][0][1]
+                    x2 = a['human_expert_odom'][index][-1][0]-a['move_base_path'][index][-1][0]
+                    y2 = a['human_expert_odom'][index][-1][1]-a['move_base_path'][index][-1][1]
+                    # if (x1!=0 or x2!=0 or y1!=0 or y2!=0):
+                        # print(filename, index, x1, y1, x2, y2)
+                    total+=1
+                    if (x2!=0 or y2!=0):
+                        print(filename, index, x2, y2)
+                        count+=1
+                    # if (a['move_base_path'][index][-1][0]>400 or a['move_base_path'][index][-1][1]>400):
+                    #     print(a['move_base_path'][index][-1][0],a['move_base_path'][index][-1][1])
+print(count,"/", total, "mismatched end positions")
